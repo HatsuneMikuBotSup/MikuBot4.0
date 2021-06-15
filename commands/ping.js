57b7a1f2335a74b10ec4e15ca31c74f3eab428ae
Konstantinos Katsimpas <konsti032003@gmail.com>
2021-06-15 12:37:54 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
   name: "ping",
   description: "this is a ping command!",
     execute(message, date) {
-        message.channel.send("Latency is: " + (Date.now() - message.createdTimestamp) + "ms\npong!");
+        message.channel.send("Latency is: " + (message.createdTimestamp - Date.now()) + "ms\npong!");
     }
 }
